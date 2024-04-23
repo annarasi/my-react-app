3d6b6efd41d168ed4bed5c42d14fffe3af7c60a4
Anil Narasipuram <anil.narasipuram@gmail.com>
2024-04-23 19:16:05 +0400

    add starter project files

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Design+Code React page',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Welcome to my page</h1>
+    <p>Design and engineering.</p>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift..</p>
+    <Link to="/page-2/">Watch the video</Link>
+  </div>
+)
+
+export default IndexPage
